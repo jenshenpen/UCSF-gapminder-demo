f63d60a7eebe54718e26257e7e310838c3fc3786
Jen Shen <yaningshen@gmail.com>
2017-03-06 14:19:44 -0800

    Plot subsets, make aggregations by country

--- code/learning_gapminder.R
+++ code/learning_gapminder.R
@@ -23,11 +23,48 @@ plot(gapM$pop,gapM$lifeExp) # basic graph
 
 plot(gapM$pop, gapM$lifeExp, main = "Gap Minder", xlab = "Population", ylab = "Life Exp")
 
-# First subset the data
-# Take gapM$pop >= 10000
+# First subset the data. gapM$pop >= 10000
+  # BP: comment on your intention of what the data is going to do
+
+gapM_large_Pop <- gapM[gapM$pop >= 100000000,]
+  # BP: be consistent with formatting 
+  # square brackets help with subsetting
+
+View(gapM_large_Pop)
+gapM$pop >= 100000000
+
 # Plot subsetted data
 
+plot(gapM_large_Pop$pop, gapM_large_Pop$lifeExp, main = "Gap Minder", xlab = "Population", ylab = "Life Exp")
 
+# Aggregate everything
 
+agg_gap_country <- aggregate(.~country, data=gapM, FUN = mean)
+  # "." take everything, all the columns
+  # "~" means "by"
+  # aggregates
+
+View(agg_gap_country)
+
+# Aggregate population by country
+agg_gap_country <- aggregate(pop~country, data=gapM, FUN = mean)
+
+View(agg_gap_country)
+
+agg_gap_country <- aggregate(cbind(pop, lifeExp)~country , data=gapM, FUN = mean)
+
+  # cbind is column bind
+  # rbind is row bind
+
+  # Note: y ~ x is formula notation in R
+  # lm(y~ x+r)
+  # lm(y~ x*r) y predicted by x, y predicted by r, and interaction
+  # lm stands for linear model
+  # anova(lm(y~x)) if x is categorical
+
+# Example of cbind
+col1 <- 1:10
+col2 <- 11:20
+cbind(col1,col2)
 
 
